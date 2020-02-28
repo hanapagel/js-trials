ad1008b2213ccbd9b70d2878b45522f0becf3f73
Your Name <you@example.com>
2020-02-28 11:51:46 -0600

    Initial commit. Completed arrays.js 1.

---
+++ arrays.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+  for (let i = 0; i < items.length; i ++) {
+    console.log(`${items[i]}${i}`);
+  }
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
+
+let thisList = ['apple', 'banana', 'carrot', 'durian'];
+
+printIndices(thisList)
